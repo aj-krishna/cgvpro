8e5f8de732385ae21ce207576fe657a54f3293da
Ajay Krishna <kiran.ajay07@gmail.com>
2018-06-01 22:53:02 +0530

    Removed sample comment

--- main.c
+++ main.c
@@ -516,8 +516,6 @@ void merge(int arr[], int l, int m, int r)
     }
 }
 
-/* l is for left index and r is right index of the
-  *  sub-array of arr to be sorted */
 void mergeSort(int arr[], int l, int r)
 {
     if (l < r)
